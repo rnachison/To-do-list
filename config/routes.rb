10dd6a85d1a793144cf42a3d3b94d9d384bae02a
rnachison <rnachison@gmail.com>
2014-11-18 15:17:30 -0800

    add user authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
 
+
   root 'home#index'
+
+    get 'signup', to: 'users#new', as: 'signup'
+    get 'login', to: 'sessions#new', as: 'login'
+    get 'logout', to: 'sessions#destroy', as: 'logout'
+
+    resources :sessions
+    resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
