9c8683ad133a948e5c204066da77a37b21119b5b
rnachison <rnachison@gmail.com>
2014-11-18 15:26:36 -0800

    add user lists to show page

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
 
     resources :sessions
     resources :users
+    resources :lists
+    resources :items
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
