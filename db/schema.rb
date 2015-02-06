46cbc620111edbbaca7078b81f4a6e87e69d1ac7
rnachison <rnachison@gmail.com>
2015-02-06 13:10:53 -0800

    update user model for auth

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141118202226) do
+ActiveRecord::Schema.define(version: 20150206210922) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,9 +38,10 @@ ActiveRecord::Schema.define(version: 20141118202226) do
 
   create_table "users", force: true do |t|
     t.string   "email"
-    t.string   "password"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
+    t.integer  "user_id"
   end
 
 end
